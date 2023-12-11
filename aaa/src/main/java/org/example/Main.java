a55957afa8cd90f3dff5442be23b1c9f7f7ee090
Aleksandr Evtyh <aleksandr.evtyh@fojin.tech>
2023-12-11 17:32:09 +0300

    aaaaaaaaaa2

--- aaa/src/main/java/org/example/Main.java
+++ aaa/src/main/java/org/example/Main.java
@@ -4,13 +4,28 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[] {1, 2, 3, 4, 5};
+        int[] array = new int[] {1231, -2322, 32321, 4232, 2};
         System.out.println(Arrays.toString(swapper(array)));
     }
 
     public static int[] swapper(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            int max =
+        if (array.length == 0) {
+            return array;
         }
+        int maxValue = Integer.MIN_VALUE, minValue = Integer.MAX_VALUE;
+        int maxIndex = 0, minIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+                minIndex = i;
+            }
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+                maxIndex = i;
+            }
+        }
+        array[maxIndex] = minValue;
+        array[minIndex] = maxValue;
+        return array;
     }
 }
